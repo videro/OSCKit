516c126aea642e334df366d50b6098eb7b9f4632
Philipp Kirchner <p.kirchner@videro.com>
2017-02-16 14:32:33 +0100

    correct sizes for array-based messages

--- OSCKit/OSCMessage.m
+++ OSCKit/OSCMessage.m
@@ -22,24 +22,38 @@ NSInteger roundUp4(NSInteger value) {
 }
 
 - (NSInteger)estimatedSize {
-  NSInteger size = roundUp4([self.address lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1);
-  
-  size += roundUp4(self.arguments.count + 2); // type string with leading comma and null
-  
-  for (NSObject *arg in self.arguments) {
-    if([arg isKindOfClass:[NSString class]]) {
-      NSString *string = (NSString*)arg;
-      size += roundUp4([string lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1);
-    } else if([arg isKindOfClass:[NSNumber class]]) {
-      size += 4;
-    } else {
-      [[NSException exceptionWithName:@"OSCProtocolException"
-        reason:[NSString stringWithFormat:@"argument is not an int, float, or string"]
-        userInfo:nil] raise];
+    NSInteger size = roundUp4([self.address lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1);
+    
+    size += roundUp4(self.arguments.count + 2); // type string with leading comma and null
+    
+    for (NSObject *arg in self.arguments) {
+        if([arg isKindOfClass:[NSString class]]) {
+            NSString *string = (NSString*)arg;
+            size += roundUp4([string lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1);
+        } else if([arg isKindOfClass:[NSNumber class]]) {
+            size += 4;
+        } else if([arg isKindOfClass:[NSArray class]]){
+            size += 2;
+            for (NSObject *arg in self.arguments) {
+                if([arg isKindOfClass:[NSString class]]) {
+                    NSString *string = (NSString*)arg;
+                    size += roundUp4([string lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1);
+                } else if([arg isKindOfClass:[NSNumber class]]) {
+                    size += 4;
+                } else {
+                    [[NSException exceptionWithName:@"OSCProtocolException"
+                                             reason:[NSString stringWithFormat:@"argument is not an int, float, or string"]
+                                           userInfo:nil] raise];
+                }
+            }
+        } else {
+            [[NSException exceptionWithName:@"OSCProtocolException"
+                                     reason:[NSString stringWithFormat:@"argument is not an int, float, or string"]
+                                   userInfo:nil] raise];
+        }
     }
-  }
-  
-  return size;
+    
+    return size;
 }
 
 @end
